a75b74fa453eeab8c0f820852df2a02561d687db
r-kamitani <r-kamitani@cnt.biglobe.co.jp>
2017-10-18 17:16:54 +0900

    Requestの処理をMainに追加

--- src/main/java/com/naosim/dddwork/api/Main.java
+++ src/main/java/com/naosim/dddwork/api/Main.java
@@ -20,16 +20,29 @@ public class Main {
             String serviceType = args[0];
 
             if(ServiceType.getEnum(serviceType).isInput()){
+                // 勤怠入力Request
+
+                InputKintaiRequest inputKintaiRequest = new InputKintaiRequest(args[1],args[2],args[3]);
+                if(inputKintaiRequest.isArgsValidate()){
+                    throw new RuntimeException("勤怠入力チェックエラー");
+                }
                 // 勤怠入力サービスを呼ぶ
                 InputKintai kintai = new InputKintai();
-                kintai.execute(new KintaiDate(args[1]), new DailyWorkingStartTime(args[2]), new DailyWorkingEndTime(args[3]));
+                kintai.execute(
+                        inputKintaiRequest.getKintaiDate(),
+                        inputKintaiRequest.getDailyWorkingStartTime(),
+                        inputKintaiRequest.getDailyWorkingEndTime());
             }
+
             if(ServiceType.getEnum(serviceType).isTotal()){
+                // 月次集計Request
+                MonthlyKintaiRequest monthlyKintaiRequest = new MonthlyKintaiRequest(args[1]);
+                if(monthlyKintaiRequest.isArgsValidate()){
+                    throw new RuntimeException("勤怠集計チェックエラー");
+                }
                 // 勤怠集計サービスを呼ぶ
                 TotalMonthlyKintai totalMonthlyKintai = new TotalMonthlyKintai();
-                // TODO
-                TargetYearMonth targetYearMonth = new TargetYearMonth(args[1]);
-                MonthlyKintai monthlyKintai = totalMonthlyKintai.execute(targetYearMonth);
+                MonthlyKintai monthlyKintai = totalMonthlyKintai.execute(monthlyKintaiRequest.getYearMonth());
 
                 // 表示
                 DisplayResult.displayResult(monthlyKintai);
